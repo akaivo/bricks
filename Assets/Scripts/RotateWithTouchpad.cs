e3947f5275efe5bdae286f6b1eb1278a32130a79
Aadam Kaivo <akaivo@ut.ee>
2018-08-04 23:27:57 +0200

    better brick rotation

--- Assets/Scripts/RotateWithTouchpad.cs
+++ Assets/Scripts/RotateWithTouchpad.cs
@@ -6,23 +6,29 @@ using Vector3 = UnityEngine.Vector3;
 
 public class RotateWithTouchpad : MonoBehaviour
 {
-	public GameObject ControllerGO;
+    public GameObject ControllerGO;
 
-	private Vector3 globalHeading = Vector3.left;
-	private SteamVR_Controller.Device device { get { return SteamVR_Controller.Input((int)ControllerGO.GetComponent<SteamVR_TrackedObject>().index); } }
-	private EVRButtonId touchPad = EVRButtonId.k_EButton_SteamVR_Touchpad;
-	
-	private void Update()
-	{
-		
-		if(device.GetTouch(touchPad) && device.GetAxis(touchPad).magnitude > 0.2f)
-		{
-			var h = device.GetAxis(touchPad);
-			globalHeading = ControllerGO.transform.TransformDirection(new Vector3(-h.x, h.y, 0f));
-			globalHeading = Vector3.ProjectOnPlane(globalHeading, Vector3.up);
-		}
-		
-		transform.rotation = Quaternion.LookRotation(globalHeading);
-			
-	}
+    private Vector3 globalHeading = Vector3.left;
+
+    private SteamVR_Controller.Device device
+    {
+        get { return SteamVR_Controller.Input((int) ControllerGO.GetComponent<SteamVR_TrackedObject>().index); }
+    }
+
+    private readonly EVRButtonId _touchPad = EVRButtonId.k_EButton_SteamVR_Touchpad;
+    private readonly float _sensitivity = 3f;
+
+    private void Update()
+    {
+        if (device.GetTouch(_touchPad))
+        {
+            globalHeading = Matrix4x4.TRS(Vector3.zero, Quaternion.AngleAxis(device.GetAxis(_touchPad).x * _sensitivity, Vector3.up), Vector3.one)
+                .MultiplyVector(globalHeading);
+            /*var h = device.GetAxis(touchPad);
+            globalHeading = ControllerGO.transform.TransformDirection(new Vector3(-h.x, h.y, 0f));
+            globalHeading = Vector3.ProjectOnPlane(globalHeading, Vector3.up);*/
+        }
+
+        transform.rotation = Quaternion.LookRotation(globalHeading);
+    }
 }
